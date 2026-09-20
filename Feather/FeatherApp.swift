fddd2489f4cda08e76c571d6b0a2bde52f7bfe4a
sideload-kor <woozn0220@gmail.com>
2026-09-20 02:48:36 +0000

    changed bundle identifier

--- Feather/FeatherApp.swift
+++ Feather/FeatherApp.swift
@@ -180,7 +180,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 				config.urlCache = nil
 				return DataLoader(configuration: config)
 			}()
-			let dataCache = try? DataCache(name: "thewonderofyou.Feather.datacache") // disk cache
+			let dataCache = try? DataCache(name: "com.sideloadkor.aurora.datacache") // disk cache
 			let imageCache = Nuke.ImageCache() // memory cache
 			dataCache?.sizeLimit = 500 * 1024 * 1024
 			imageCache.costLimit = 100 * 1024 * 1024
